4cd5fa8155fe4868ddd12bd687fa22eeb6f837b3
Rickard Dahlstrom <dahlstrom.rickard@gmail.com>
2020-03-29 09:53:48 +1300

    Scrolling

--- public/js/layers.js
+++ public/js/layers.js
@@ -1,23 +1,63 @@
 export function createBackgroundLayer(level, sprites) {
+    const tiles = level.tiles
+    const resolver = level.tileCollider.tiles
+
     const buffer = document.createElement('canvas');
-    buffer.width = 256;
+    buffer.width = 2048;
     buffer.height = 240;
 
     const context = buffer.getContext('2d');
 
-    level.tiles.forEach((tile, x, y) => {
-        sprites.drawTile(tile.name, context, x, y);
-    });
+    let startIndex, endIndex;
+    function redraw(drawFrom, drawTo) {
+        if (drawFrom === startIndex && drawTo === endIndex) {
+            return; // Optimization, prevents drawing if background hasn't moved. 
+        }
 
-    return function drawBackgroundLayer(context) {
-        context.drawImage(buffer, 0, 0);
+        startIndex = drawFrom;
+        endIndex = drawTo;
+
+        console.log('redraw')
+
+        for (let x = startIndex; x <= endIndex; ++x) {
+            const col = tiles.grid[x]
+            if (col) {
+                col.forEach((tile, y) => {
+                    sprites.drawTile(tile.name, context, x - startIndex, y);
+                })
+            }
+
+        }
+    }
+
+    return function drawBackgroundLayer(context, camera) {
+        const drawWidth = resolver.toIndex(camera.size.x)
+        const drawFrom = resolver.toIndex(camera.pos.x)
+        const drawTo = drawFrom + drawWidth
+        redraw(drawFrom, drawTo)
+        context.drawImage(
+            buffer,
+            -camera.pos.x % 16,
+            -camera.pos.y);
     };
 }
 
-export function createSpriteLayer(entities) {
+export function createSpriteLayer(entities, width = 64, height = 64) {
+    const spriteBuffer = document.createElement('canvas');
+    spriteBuffer.width = width;
+    spriteBuffer.height = height;
+    const spriteBufferContext = spriteBuffer.getContext('2d');
+
     return function drawSpriteLayer(context) {
         entities.forEach(entity => {
-            entity.draw(context);
+            spriteBufferContext.clearRect(0, 0, width, height)
+
+            entity.draw(spriteBufferContext);
+
+            context.drawImage(
+                spriteBuffer,
+                entity.pos.x - camera.pos.x,
+                entity.pos.y - camera.pos.y);
         });
     };
 }
@@ -33,15 +73,13 @@ export function createCollisionLayer(level) {
         resolvedTiles.push({ x, y });
         return getByIndexOriginal.call(tileResolver, x, y);
     }
-    console.log('1')
-    return function drawCollision(context) {
-        console.log('2')
+    return function drawCollision(context, camera) {
         context.strokeStyle = 'blue';
         resolvedTiles.forEach(({ x, y }) => {
             context.beginPath();
             context.rect(
-                x * tileSize,
-                y * tileSize,
+                x * tileSize - camera.pos.x,
+                y * tileSize - camera.pos.y,
                 tileSize, tileSize);
             context.stroke();
         });
@@ -50,11 +88,26 @@ export function createCollisionLayer(level) {
         level.entities.forEach(entity => {
             context.beginPath();
             context.rect(
-                entity.pos.x, entity.pos.y,
-                entity.size.x, entity.size.y);
+                entity.pos.x - camera.pos.x,
+                entity.pos.y - camera.pos.y,
+                entity.size.x,
+                entity.size.y);
             context.stroke();
         });
 
         resolvedTiles.length = 0;
     };
+}
+
+export function createCameraLayer(cameraToDraw) {
+    return function drawCameraRect(context, fromCamera) {
+        context.strokeStyle = 'purple'
+        context.beginPath();
+        context.rect(
+            cameraToDraw.pos.x - fromCamera.pos.x,
+            cameraToDraw.pos.y - fromCamera.pos.y,
+            cameraToDraw.size.x,
+            cameraToDraw.size.y);
+        context.stroke();
+    }
 }
--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,10 @@
 import Timer from './Timer.js';
+import Camera from './camera.js';
 import { loadLevel } from './loaders.js';
 import { createMario } from './entities.js';
-import { createCollisionLayer } from './layers.js';
+import { createCollisionLayer, createCameraLayer } from './layers.js';
 import { setupKeyboard } from './input.js';
+import { setUpMouseControl } from './debug.js';
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
@@ -12,30 +14,28 @@ Promise.all([
     loadLevel('1-1'),
 ])
     .then(([mario, level]) => {
+        const camera = new Camera()
+        window.camera = camera
+
         mario.pos.set(64, 64);
 
-        createCollisionLayer(level)
+        level.comp.layers.push(
+            createCollisionLayer(level),
+            createCameraLayer(camera)
+        );
 
         level.entities.add(mario);
 
         const input = setupKeyboard(mario);
         input.listenTo(window);
 
-        ['mousedown', 'mousemove'].forEach(eventName => {
-            canvas.addEventListener(eventName, event => {
-                if (event.buttons === 1) {
-                    mario.vel.set(0, 0);
-                    mario.pos.set(event.offsetX, event.offsetY);
-                }
-            });
-        });
-
+        setUpMouseControl(canvas, mario, camera)
 
         const timer = new Timer(1 / 60);
         timer.update = function update(deltaTime) {
             level.update(deltaTime);
 
-            level.comp.draw(context);
+            level.comp.draw(context, camera);
         }
 
         timer.start();
